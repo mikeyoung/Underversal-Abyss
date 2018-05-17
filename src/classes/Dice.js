2ce80c90d2a8f448125c8c262217b93955fc6508
Mike Young <mcy001@gmail.com>
2018-05-16 22:13:05 -0500

    fixed dice logic

--- src/classes/Dice.js
+++ src/classes/Dice.js
@@ -1,8 +1,14 @@
 export default class Dice {
   static roll (dice) {
+    let total = 0
     dice = dice.toLowerCase()
     let count = parseInt(dice.split('d')[0])
     let sides = parseInt(dice.split('d')[1])
-    return count * (Math.floor(Math.random() * sides) + 1)
+
+    for (let i = 0; i < count; i++) {
+      total += Math.floor(Math.random() * sides) + 1
+    }
+
+    return total
   }
 }
